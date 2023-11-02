c45215142c7b5924441570de8873083e16148db3
Joshua Töpfer <joshua.toepfer@innoq.com>
2023-11-02 10:34:36 +0100

    test ci

--- python/unittest/todo_project_name_test.py
+++ python/unittest/todo_project_name_test.py
@@ -3,5 +3,8 @@ import unittest
 
 class TodoProjectNameTestCase(unittest.TestCase):
 
-    def test_python_test_setup_works(self):
+    def test_python_test_setup_fails(self):
         self.assertEqual(42, 43)
+
+    def test_python_test_setup_works(self):
+        self.assertEqual(42, 42)
